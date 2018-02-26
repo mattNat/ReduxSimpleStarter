f1243943a0e40c3df2814ce4d3acb2f091d75237
mattNat <mattsanpedro76@gmail.com>
2018-02-25 20:33:07 -0800

    add SearchBar in /components/search_bar.js

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchBar = () => {
+  // generate HTML user input for typing into
+  return <input />;
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDlNXT6U3rbExS2kiZj_gBzkyHD8h81d2E';
+
 // Create a new component.  This component should produce some HTML
 
 // JSX cannot be interpretted by the browser
 // ALWAYS one component per file!
 const App = () => {
   // some JSX gets transpiled to vanilla JS
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
