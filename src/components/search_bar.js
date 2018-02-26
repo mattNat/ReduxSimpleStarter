9343b1e4339fc7c5cdc31bf3aa7538e8998a704a
mattNat <mattsanpedro76@gmail.com>
2018-02-25 21:04:47 -0800

    add up to section 1-16

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+// const Component = React.Component
 
-const SearchBar = () => {
-  // generate HTML user input for typing into
-  return <input />;
-};
+// // functional component
+// const SearchBar = () => {
+//   // generate HTML user input for typing into
+//   return <input />;
+// };
+
+// def a new class and give it access to all functionality that React.Component has
+class SearchBar extends Component {
+  render() {
+    // pass event handler to element
+    // all HTML input elements emit change event
+    // JSX and JS variables -> wrap it with curly braces
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+
+  // declare event handler
+  // name of element (Input), name of event (Change)
+  // event object to get more info 
+  
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
 
 export default SearchBar;
