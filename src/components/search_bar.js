c2b37361b8b7a95bbb93c8119c1796009e105a9d
mattNat <mattsanpedro76@gmail.com>
2018-02-25 21:40:30 -0800

    add up to section 1-18 lecture on state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,17 +9,32 @@ import React, { Component } from 'react';
 
 // def a new class and give it access to all functionality that React.Component has
 class SearchBar extends Component {
+  // all JS classes have constructor
+  // set up
+  constructor(props) {
+    // call on parent method with super
+    super(props);
+    // initialize with new object with properties we want to record on state
+    this.state = { term: '' };
+  }
+
   render() {
     // pass event handler to element
     // all HTML input elements emit change event
     // JSX and JS variables -> wrap it with curly braces
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      // always manipulate state like this
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        {/* Value of the input: {this.state.term} */}
+      </div>
+    );
   }
 
   // declare event handler
   // name of element (Input), name of event (Change)
   // event object to get more info 
-  
+
   // onInputChange(event) {
   //   console.log(event.target.value);
   // }
