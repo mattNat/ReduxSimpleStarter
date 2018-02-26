1be0524d4817850760c34f20607943f34b6dc5e2
mattNat <mattsanpedro76@gmail.com>
2018-02-25 20:10:46 -0800

    add up to section 1-12

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component.  This component should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// JSX cannot be interpretted by the browser
+// ALWAYS one component per file!
+const App = () => {
+  // some JSX gets transpiled to vanilla JS
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in the DOM)
+// self-closing tab, we need to pass an instance, not a class
+ReactDOM.render(<App />, document.querySelector('.container'));
